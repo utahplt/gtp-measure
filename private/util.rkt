6944b48362c8b8489086b7b187756fd9b69e7acc
Ben Greenman <benjaminlgreenman@gmail.com>
2018-02-05 14:00:40 -0500

    private: add gtp-measure log interceptor
    Not using this now (and probably should avoid using it --- working with
    loggers takes up a lot of time), but this macro is closer to something
    for a library than my last attempts

--- private/util.rkt
+++ private/util.rkt
@@ -13,7 +13,9 @@
   log-gtp-measure-error
   log-gtp-measure-warning
   log-gtp-measure-info
-  log-gtp-measure-debug)
+  log-gtp-measure-debug
+
+  force/gtp-measure)
 
 (require
   file/glob
@@ -21,11 +23,39 @@
   (only-in racket/file
     delete-directory/files)
   (only-in racket/path
-    file-name-from-path))
+    file-name-from-path)
+  (only-in racket/logging
+    with-intercepted-logging)
+  (for-syntax
+    racket/base
+    (only-in racket/syntax format-id)
+    syntax/parse))
 
 ;; =============================================================================
 
 (define-logger gtp-measure)
+
+(define-syntax (make-log-interceptor stx)
+  (syntax-parse stx
+   [(_ ?base-name:id)
+    #:with ?logger-id (format-id stx "~a-logger" (syntax-e #'?base-name))
+    #'(lambda (thunk #:level [level 'info])
+        (define inbox (make-hasheq '((debug . ()) (info . ()) (warning . ()) (error . ()) (fatal . ()))))
+        (with-intercepted-logging
+          (λ (l)
+            (define lvl (vector-ref l 0))
+            (define msg (vector-ref l 1))
+            (when (eq? '?base-name (vector-ref l 3))
+              (hash-set! inbox lvl (cons msg (hash-ref inbox lvl))))
+            (void))
+          thunk
+          #:logger ?logger-id
+          level)
+      (for/hasheq ([(k v) (in-hash inbox)])
+        (values k (reverse v))))]))
+
+(define force/gtp-measure
+  (make-log-interceptor gtp-measure))
 
 ;; =============================================================================
 
